8e8ddd71a14170b68121681f13072f3df720bdaa
Jiří Krudenc <krudencj24@spstrutnov.cz>
2026-04-14 10:08:17 +0200

    Úvodní commit

---
+++ SkolniPortal/SkolniPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SkolniPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
