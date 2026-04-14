802fcf81aff6c33b3a50045aafa3c0e611d1b76d
Jiří Krudenc <krudencj24@spstrutnov.cz>
2026-04-14 10:33:07 +0200

    Připravení celého repozitáře

---
+++ SkolniPortal/SkolniPortal/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SkolniPortal.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+
+}
