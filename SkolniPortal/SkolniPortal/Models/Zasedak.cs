7dd6ca69cb71edc7ba35bab088a2970400f535f0
Jiří Krudenc <krudencj24@spstrutnov.cz>
2026-05-26 11:08:08 +0200

    Model pro zasedací pořádek
    Tohle je jen první verze zatím bez migrace

---
+++ SkolniPortal/SkolniPortal/Models/Zasedak.cs
@@ -0,0 +1,40 @@
+﻿namespace SkolniPortal.Models
+{
+    public class Zasedak
+    {
+        public int Id { get; set; }
+        public int pocetMist { get; set; }
+        public string  forTrida { get; set; }
+        public string M1 { get; set; }
+        public string M2 { get; set; }
+        public string M3 { get; set; }
+        public string M4 { get; set; }
+        public string M5 { get; set; }
+        public string M6 { get; set; }
+        public string M7 { get; set; }
+        public string M8 { get; set; }
+        public string M9 { get; set; }
+        public string M10 { get; set; }
+        public string M11 { get; set; }
+        public string M12 { get; set; }
+        public string M13 { get; set; }
+        public string M14 { get; set; }
+        public string M15 { get; set; }
+        public string M16 { get; set; }
+        public string M17 { get; set; }
+        public string M18 { get; set; }
+        public string M19 { get; set; }
+        public string M20 { get; set; }
+        public string M21 { get; set; }
+        public string M22 { get; set; }
+        public string M23 { get; set; }
+        public string M24 { get; set; }
+        public string M25 { get; set; }
+        public string M26 { get; set; }
+        public string M27 { get; set; }
+        public string M28 { get; set; }
+        public string M29 { get; set; }
+        public string M30 { get; set; }
+
+    }
+}
